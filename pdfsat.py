da677964e2e4ac18874544019eea343882ecc8a1
Wil <wilfried.elmenreich@aau.at>
2025-11-12 15:41:12 +0100

    v33

--- pdfsat.py
+++ pdfsat.py
@@ -12,7 +12,7 @@ Features:
 - Screen blanking
 
 Wilfried Elmenreich
-Version 0.32, November 2025
+Version 0.33, November 2025
 Released under the WTFPL (Do What The Fuck You Want To Public License)
 """
 
